488fc494e8503dd990f86e55e9d2344b5cab5ebc
Dan Cline <6798349+Rjected@users.noreply.github.com>
2025-11-12 12:32:38 -0500

    feat: add StaticFileSegment::AccountChangeSets
    chore: add changeset info to segment header
    
    feat(provider): implement append_account_changeset
    
    chore: formatting and more TODOs
    
    chore: add stub for account_before_block
    
    fix: use proper value for account changeset static files
    
    feat: impl binary search for address in changeset
    
    feat: finish writer fn, add tests
    
    chore: add fallback to db when not found in static files
    
    feat: add account_changesets_range to ChangesetReader
    
    feat: add support for account changesets in IndexAccountHistory
    
    feat: add support for account changesets in static file producer
    
    chore: serialization changes
    
    feat: add support for account changesets in db get
    
    chore: docs and attempt to fix db get
    
    fix: properly upgrade segment header serialize / deserialize
    
    feat: add read-only segments
    
    feat: add cli flag for enabling v2 static files
    
    chore: remove Segment for AccountChangeSets
    
    chore: add unreachable! for unreachable db get branch
    
    chore: remove noisy traces
    
    chore: make binary search more concise
    
    feat: add support for account changeset static file pruning
    
    feat: make HashedPostState::from_reverts work with static files
    
    fix: don't write to db
    
    chore: update book cli
    
    chore: make clippy happy
    
    chore: fix test compilation
    
    chore: fix feat propagation
    
    feat: add StaticFileRangeWalker skeleton
    
    fix: fix inclusive ranges
    
    feat: impl removal for changesets in exec unwind
    
    chore: replace prefixsetloader with fn that uses provider
    
    chore(trie): propagate errors from root methods
    
    chore: make clippy happy
    
    chore: no underscores
    
    chore(static-file): call increment_block when appending account changesets
    
    chore(merkle-changesets): add logs for changeset reverts
    
    fix: return 0 for start

--- crates/node/core/src/args/static_files.rs
+++ crates/node/core/src/args/static_files.rs
@@ -18,6 +18,10 @@ pub struct StaticFilesArgs {
     /// Number of blocks per file for the receipts segment.
     #[arg(long = "static-files.blocks-per-file.receipts")]
     pub blocks_per_file_receipts: Option<u64>,
+
+    /// Number of blocks per file for the account changesets segment.
+    #[arg(long = "static-files.blocks-per-file.account-change-sets")]
+    pub blocks_per_file_account_change_sets: Option<u64>,
 }
 
 impl StaticFilesArgs {
@@ -31,6 +35,9 @@ impl StaticFilesArgs {
                     .blocks_per_file_transactions
                     .or(config.blocks_per_file.transactions),
                 receipts: self.blocks_per_file_receipts.or(config.blocks_per_file.receipts),
+                account_change_sets: self
+                    .blocks_per_file_account_change_sets
+                    .or(config.blocks_per_file.account_change_sets),
             },
         }
     }
